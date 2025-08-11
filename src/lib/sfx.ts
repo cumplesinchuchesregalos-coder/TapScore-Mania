41e8304ea4cbc456462b6bd7d965290a4b9b07e5
Cumple Sin Chuches Regalos <cumplesinchuchesregalos@gmail.com>
2025-08-11 00:58:42 +0000

    no se olle nada

--- src/lib/sfx.ts
+++ src/lib/sfx.ts
@@ -3,6 +3,6 @@
 // Example: export const SFX = { tap: 'data:audio/wav;base64,....' };
 
 export const SFX = {
-    tap: "",
-    combo: ""
+    tap: "data:audio/wav;base64,UklGRjwAAABXQVZFZm10IBAAAAABAAEARKwAAIhYAQACABAAZGF0YSAgAAEA",
+    combo: "data:audio/wav;base64,UklGRlIAAABXQVZFZm10IBAAAAABAAEARKwAAIhYAQACABAAZGF0YSBIAAAAP3///wH+/gD9/f4A/Pz+APv8/gD5/P4A9vz+APb8/gD0/P4A7/z+AOv8/gDo/P4A4fz+AN78/gDb/P4A2fz+ANj8/gDU/P4A0vz+AMP8/gC+/P4Atvz+ALb8/gC0/P4Asfz+AK/8/gCr/P4Aqfz+AKf8/gCj/P4An/z+AJz8/gCZ/P4Al/z+AJf8/gCU/P4A"
 }
