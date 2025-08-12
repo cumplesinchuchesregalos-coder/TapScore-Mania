e753a9a424b83349781b0cc38dcad3bf16f82ea5
Cumple Sin Chuches Regalos <cumplesinchuchesregalos@gmail.com>
2025-08-12 00:35:26 +0000

    Configura Next.js para exportación estática

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Adds the 'export' output option
+
+  // Any existing configuration options would go here
+  // For example:
+  // reactStrictMode: true,
+  // swcMinify: true,
+  // images: {
+  //   unoptimized: true,
+  // },
+};
+
+module.exports = nextConfig;
